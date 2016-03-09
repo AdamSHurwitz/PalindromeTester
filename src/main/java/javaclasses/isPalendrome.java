e9583396af1c8f5f2c1eb725d9d81d39b356513c
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-08 21:46:39 -0800

    feat: solution solved

--- src/main/java/javaclasses/isPalendrome.java
+++ src/main/java/javaclasses/isPalendrome.java
@@ -8,17 +8,22 @@ public class isPalendrome {
     static String s = "alpha";
 
     public static void main(String[] arg) {
-        System.out.println("Answer is: " + isPalendrome(s));
+        System.out.println("Palendrome is: " + isPalendrome(s));
     }
 
     public static boolean isPalendrome(String s) {
-        if (s.length() == 0 || s.length() == 1) {
-            return true;
+        String string = s;
+        System.out.println("length - " + string.length() + " | first - "
+                + string.substring(0, 1) + " | last - "
+                + string.substring(string.length() - 1) + " | full - " + string);
+
+        while (string.length() > 1) {
+            if (!string.substring(0, 1).equals(string.substring(string.length() - 1))) {
+                return false;
+            }
+            string = string.substring(1, string.length() - 1);
         }
-        else if (!s.substring(0,1).equals(s.substring(s.length()-1))){
-            System.out.println("first and last are not equal");
-            return false;
-        }
-        else {return false;}
+        System.out.println("string - " + string);
+        return true;
     }
 }
