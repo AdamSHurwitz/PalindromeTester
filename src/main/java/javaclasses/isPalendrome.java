9a72e4893b7da740e34a76fdf5f4d77338e469d1
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-08 20:31:42 -0800

    feat: base case for first and last letter of string not being equal

--- src/main/java/javaclasses/isPalendrome.java
+++ src/main/java/javaclasses/isPalendrome.java
@@ -5,7 +5,7 @@ package javaclasses;
  */
 public class isPalendrome {
     static private final String LOG_TAG = isPalendrome.class.getSimpleName();
-    static String s = "a";
+    static String s = "alpha";
 
     public static void main(String[] arg) {
         System.out.println("Answer is: " + isPalendrome(s));
@@ -14,6 +14,11 @@ public class isPalendrome {
     public static boolean isPalendrome(String s) {
         if (s.length() == 0 || s.length() == 1) {
             return true;
-        } else {return false;}
+        }
+        else if (!s.substring(0,1).equals(s.substring(s.length()-1))){
+            System.out.println("first and last are not equal");
+            return false;
+        }
+        else {return false;}
     }
 }
